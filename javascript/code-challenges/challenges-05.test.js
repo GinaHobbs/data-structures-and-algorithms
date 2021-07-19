9c0d4ca99a3290c9951c96fd81ceb6493dd9fa05
GinaHobbs <ginamariehobbs@gmail.com>
2021-07-19 09:16:50 -0500

    challenge 1 complete

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -27,7 +27,7 @@ Write a function named addValues that, given an array of numbers as input, uses 
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr) => {
-  // Solution code here...
+  //let newArr = arr.reduce()
 };
 
 /* ------------------------------------------------------------------------------------------------
